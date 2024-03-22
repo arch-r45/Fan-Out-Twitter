725a40a65b6d0eea02b8133e26543a7ad9df26fc
RJ Murphy <rjmurphy777@gmail.com>
2024-03-21 22:33:34 -0400

    Added Follow User View

--- request2.py
+++ request2.py
@@ -1,6 +1,6 @@
 import requests
 
-query_par = {"username": "dumer", "password": "barktree"}
+query_par = {"username": "dumber", "password": "barktree"}
 response = requests.post("http://127.0.0.1:8000/signin", params=query_par)
 
 print(response.text)
--- views.py
+++ views.py
@@ -38,21 +38,46 @@ class UserModel():
 
 @app.post('/signup')
 def register(username:str, password:str):
+    "Check if Username Already Exists"
     user = UserModel(username, password)
     cur.execute("INSERT INTO Users (username, password) VALUES (?, ?)", (user.username, user.password))
     con.commit()
     return {"status": "Success", "message": "User registered successfully"}
-
-
 
 @app.post('/signin')
 def signin(username: str, password: str):
     user = UserModel(username, password)
     res = cur.execute("SELECT username, password FROM Users WHERE (username = ? and password = ?)", (user.username, user.password))
     user_record = res.fetchone()
-    print(user_record)
-    if user_record== None:
+    if user_record == None:
         return {"status": "Failure", "message": "User Password Incorrect"} 
     
     else:
         return {"status": "Success",  "message": "User successfully authenticated."}
+
+@app.post('/follow')
+def follow(user_id: str, follower_id: str):
+    print(user_id, follower_id)
+    res = cur.execute("SELECT username FROM Users WHERE (username = ?)", (user_id,))
+    user = res.fetchone()
+    user = user[0]
+    followee_res = cur.execute("SELECT username FROM Users WHERE (username = ?)", (follower_id,))
+    followee_res = followee_res.fetchone()
+    followee_user = followee_res[0] if followee_res is not None else None
+    if followee_res != None:
+        cur.execute("INSERT INTO Followers (username, followee_id) VALUES (?, ?)", (user, followee_user))
+        con.commit()
+        return {"status": "Success", "message": f"You Succesfully Followed {followee_user}"}
+
+    else:
+        return {"status": "Failure", "message": "This Person You are trying to follow does not exist"}  
+
+
+
+
+
+
+
+
+    
+
